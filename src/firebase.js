7c227069d08fd0150ffac985af1f9df5e20b5389
alex-morcg <alexmorcegofijo@gmail.com>
2026-01-23 20:52:58 +0100

    Fix typo in authDomain value

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getFirestore, collection, doc, setDoc, onSnapshot, deleteDoc } from 'fi
 // IMPORTANTE: Reemplaza estos valores con los de tu proyecto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCmoiIKec5NIDYzyY_TqwXgjbI_pu4WUy8",
-  authDomain: "patrick-masajes.firebaseapp.com"",
+  authDomain: "patrick-masajes.firebaseapp.com",
   projectId: "patrick-masajes",
   storageBucket: "patrick-masajes.firebasestorage.app",
   messagingSenderId: "148332805394",
